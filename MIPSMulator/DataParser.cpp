d0dfadf9c490f6cb8465e2ce5278d79faa24a529
Alessio Rosiello <alex.robassomero@gmail.com>
2023-03-14 07:50:23 +0100

    code style changes.

--- MIPSMulator/DataParser.cpp
+++ MIPSMulator/DataParser.cpp
@@ -3,16 +3,19 @@
 
 BinaryData::BinaryData(const Path& p) {
     constexpr size_t wordline_sz = sizeof(uint64_t);
-    auto file = File{p};
+    File file{p};
     file.open(OpenFileMode::Read);
     auto lines_or_error = file.read_all();
-    if (lines_or_error.is_error()) { return; }
-    auto lines = lines_or_error.to_ok();
-    data.resize(lines.size() * wordline_sz);
+    if (lines_or_error.is_error()) {
+        // TODO: deal with error
+        return;
+    }
+    auto filedata = lines_or_error.to_ok();
+    data.reserve(filedata.size() * wordline_sz);
     for (const auto& [idx, val] :
-         lines.split_view("\n").iter().filter(&StringView::size).map(StrViewToU64Hexadecimal).enumerate()) {
+         filedata.view().split("\n").iter().filter(&StringView::size).map(StrViewToU64Hexadecimal).enumerate()) {
         for (size_t i = 0; i < wordline_sz; i++) {
-            data[(idx * wordline_sz) + i] = ((val >> (i * wordline_sz)) & 0xFF);
+            data.append((val >> (i * wordline_sz)) & 0xFF);
         }
     }
     data.resize(0x400);
